c351d03f4d0df0809e3e8ce65b7c889bc6bc62a9
Vituinho <trabalhovictor531@gmail.com>
2025-09-25 10:56:06 -0300

    Um pouco sobre MVC, Preparação da estrutura do projeto e Composer e Autoload PSR-4

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'victormoraes/miniframework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b137266f2d606b51158b6370f964b417df8fb2d9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'victormoraes/miniframework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b137266f2d606b51158b6370f964b417df8fb2d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
