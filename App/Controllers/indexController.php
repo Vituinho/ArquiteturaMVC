3e6c6290369508995a3ec7e98d1ccc2650a758a6
Vituinho <trabalhovictor531@gmail.com>
2025-09-29 12:00:08 -0300

    Feat: commit antes de vazar (4 dia seguido sem conseguir estudar, estou ficando louco)

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -1,5 +1,17 @@
 <?php
 
+namespace App\Controllers\
+
+class IndexController {
     
+    public function index() {
+        echo 'Chegamos e disparamos';
+    }
+    
+    public function sobreNos() {
+        echo 'Chegamos e disparamos 2';
+    }
+
+}
 
 ?>
